9d3fc7fdaf8daac61ad26df1dcc599bdbc790aeb
Dalibor Hon <hon.dalibor@gmail.com>
2023-12-28 11:37:40 +0100

    Add auto generate of OG images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import * as i18n from "./src/i18n/consts"
+import * as i18n from "./src/i18n/consts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,19 +13,22 @@ export default defineConfig({
     prefetch: true,
     markdown: {
         shikiConfig: {
-          // Choose from Shiki's built-in themes (or add your own)
-          // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-          theme: 'material-theme-palenight',
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: "material-theme-palenight",
         },
-      },
+    },
     i18n: {
         defaultLocale: i18n.defaultLocale,
         locales: i18n.locales,
         routing: {
-            prefixDefaultLocale: false
-        }
+            prefixDefaultLocale: false,
+        },
     },
-    integrations: [
-        react()
-    ],
+    integrations: [react()],
+    vite: {
+        optimizeDeps: {
+            exclude: ["@resvg/resvg-js"],
+        },
+    },
 });

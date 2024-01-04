1abe19183f1d63110de2657356c6f301c96aa08c
Dalibor Hon <dalibor.hon@webcom.cz>
2024-01-04 14:55:50 +0100

    getOgImage - return relative URL instead of absolute

--- src/utils/getOgImageUrl.ts
+++ src/utils/getOgImageUrl.ts
@@ -5,5 +5,7 @@ import { getBlogPostSlug } from "./getBlogPostSlug";
 
 export function getOgImageUrl(locale: string, post: CollectionEntry<"posts">, url: URL) {
     const ogImageUrl = typeof post.data.ogImage === "string" ? post.data.ogImage : post.data.ogImage?.src;
-    return new URL(ogImageUrl ?? removeTrailingSlash(getRelativeLocaleUrl(locale, `/blog/posts/${getBlogPostSlug(locale, post)}.png`)), url.origin).href;
+
+    //return new URL(ogImageUrl ?? removeTrailingSlash(getRelativeLocaleUrl(locale, `/blog/posts/${getBlogPostSlug(locale, post)}.png`)), url.origin).href;
+    return removeTrailingSlash(getRelativeLocaleUrl(locale, `/blog/posts/${getBlogPostSlug(locale, post)}.png`));
 }

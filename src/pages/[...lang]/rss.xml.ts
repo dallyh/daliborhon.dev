4edea944e49a00aa2ec50f31297c48d8cfdfcef1
Dalibor Hon <hon.dalibor@gmail.com>
2024-04-02 15:02:34 +0200

    [CI-Skip] Revert "RSS: Test img media tag"
    This reverts commit 4ff4b87e61235bbd38b50c6f950b6597a3d3f8a2.

--- src/pages/[...lang]/rss.xml.ts
+++ src/pages/[...lang]/rss.xml.ts
@@ -7,18 +7,11 @@ import { parseJSONToHTML } from "@caisy/rich-text-html-parser";
 
 export { getStaticPaths } from "@i18n/utils";
 
-function createImgTag(src: string, alt: string) {
-    return `<img src="${src}" alt="${alt}">`;
-}
-
-
 export async function GET({ site, currentLocale }: APIContext) {
     const allBlogArticles = await getAllBlogArticlesByLocale({ locale: currentLocale! });
 
     const items = allBlogArticles.map((post) => {
-        let content = parseJSONToHTML(post.text?.json);
-        const img = createImgTag("https://assets.caisy.io/assets/1f552205-bf6f-43f0-8ed1-08f0d9fdccf4/d926400e-1020-406c-8d08-d7257a459f43/d8e6a398-f09e-41ae-a98d-61c179083b63/astro.jpg?original", "test");
-        content = content + img;
+        const content = parseJSONToHTML(post.text?.json);
 
         return {
             title: post.title!,

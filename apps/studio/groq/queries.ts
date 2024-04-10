4b9935f340d14c9eb1458da8d9798a8adfb34fb4
Dalibor Hon <hon.dalibor@gmail.com>
2024-04-11 00:49:05 +0200

    Add more queries

--- apps/studio/groq/queries.ts
+++ apps/studio/groq/queries.ts
@@ -17,6 +17,8 @@ const imageMetadataFragment = `
 "blurHash": asset->metadata.blurHash,
 "lqip": asset->metadata.lqip,
 "url": asset->url,
+"title": asset->title,
+"description": asset->description,
 "altText": asset->altText,
 "extension": asset->extension
 `;
@@ -43,7 +45,7 @@ body,
 
 function createTranslationReference(metadata: string) {
     const translationMetadata = `
-  "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
+    "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
       ${metadata}
     }`;
 
@@ -52,29 +54,74 @@ function createTranslationReference(metadata: string) {
 
 /* Queries */
 
+/* Posts */
 export const allPostsQuery = groq`
-*[_type == "post" && language == $language && _id > $lastId] | order(_id) [0...100] {
+*[_type == "post" && language == $language && _id > $lastId] | order(_id) [0...$maxItems] | order(publishedAt desc) {
   ${postMetaFragment}
+  ${createTranslationReference(`"slug": slug.current`)}
 }
 `;
 
-// TODO
+export const recentPostsQuery = groq`
+*[_type == "post" && language == $language] | order(_id) [0...$maxItems] | order(publishedAt desc) {
+  ${postMetaFragment}
+  ${createTranslationReference(`"slug": slug.current`)}
+}
+`;
+
 export const allPostsByCategory = groq`
-*[_type == "post" && language == $language && _id > $lastId] | order(_id) [0...100] {
+*[_type == "post" && language == $language && _id > $lastId && references($categoryId)] | order(_id) [0...$maxItems] | order(publishedAt desc) {
   ${postMetaFragment}
+  ${createTranslationReference(`"slug": slug.current`)}
 }
 `;
 
-// TODO
 export const allPostsByTag = groq`
-*[_type == "post" && language == $language && _id > $lastId] | order(_id) [0...100] {
+*[_type == "post" && language == $language && _id > $lastId && count(tags[value == "astro"]) > 0] | order(_id) [0...$maxItems] | order(publishedAt desc) {
   ${postMetaFragment}
+  ${createTranslationReference(`"slug": slug.current`)}
 }
 `;
 
-// TODO
-export const allCategoriesQuery = groq`
-*[_type == "category" && language == $language && _id > $lastId] | order(_id) [0...100] {
+export const allFeaturedPosts = groq`
+*[_type == "post" && language == $language && featured == true && _id > $lastId] | order(_id) [0...$maxItems] | order(publishedAt desc) {
+  ${postMetaFragment}
+  ${createTranslationReference(`"slug": slug.current`)}
+}
+`;
+
+/* Tags */
+// TBD
+/*
+{
+  "tags": array::join(*[tags != null] { 
+    "tags": array::join(tags[].value, ";")
+  }.tags, ";")
+}
+*/
+
+/* Categories */
+export const allCategoriesByLanguagQuery = groq`
+*[_type == "category"] | order(title[_key == $language][0].value desc) {
   ${categoryMetaFragment}
+}
+`;
+
+/* Projects */
+export const allProjectsQuery = groq`
+*[_type == "project"] | order(_id) [0...$maxItems] | order(projectStartDate desc) {
+  _id,
+  "title": title[_key == $language][0].value,
+  "description": description[_key == $language][0].value,
+  projectStartDate,
+  icon {
+    icon,
+    metadata {
+      size,
+      collectionId,
+      inlineSvg,
+      color
+    }
+  },
 }
 `;

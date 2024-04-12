20534d48a32ef83691275c094b50dc26cbcf4324
Dalibor Hon <hon.dalibor@gmail.com>
2024-04-12 13:30:57 +0200

    Fix remaining components

--- apps/frontend/src/services/graphql/fragments/tagMeta.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-const f_tagMeta = gql`
-    fragment tagMeta on Caisy_Field_Tag {
-        color
-        id
-        name
-    }
-`;
--- apps/frontend/src/utils/getAbsoluteBlogPostUrl.ts
+++ apps/frontend/src/utils/getAbsoluteBlogPostUrl.ts
@@ -1,7 +1,6 @@
 import { getAbsoluteLocaleUrl } from "astro:i18n";
 import { getBlogPostSlug } from "./getBlogPostSlug";
-import type { IGenBlogArticleMetaFragment } from "@services/graphql/__generated/sdk";
 
-export function getAbsoluteBlogPostUrl(locale: string, post: IGenBlogArticleMetaFragment) {
-    return getAbsoluteLocaleUrl(locale, `blog/posts/${getBlogPostSlug(locale, post)}/`);
+export function getAbsoluteBlogPostUrl(locale: string, slug: string) {
+    return getAbsoluteLocaleUrl(locale, `blog/posts/${slug}/`);
 }

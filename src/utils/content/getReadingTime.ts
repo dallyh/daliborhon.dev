c906f0b4e1ba22b627ae7645399de906cb1948a0
Dalibor Hon <hon.dalibor@gmail.com>
2024-12-16 00:09:05 +0100

    Add reading time to blog

---
+++ src/utils/content/getReadingTime.ts
@@ -0,0 +1,17 @@
+import calculateReadingTime from "reading-time";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toString } from "mdast-util-to-string";
+
+// https://jahir.dev/blog/astro-reading-time
+export const getReadingTime = (text: string): string | undefined => {
+	if (!text || !text.length) return undefined;
+	try {
+		const { minutes } = calculateReadingTime(toString(fromMarkdown(text)));
+		if (minutes && minutes > 0) {
+			return `${Math.ceil(minutes)}`;
+		}
+		return undefined;
+	} catch (e) {
+		return undefined;
+	}
+};

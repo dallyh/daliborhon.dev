63aea113b831fbe5c2f790f3d362231993c5639b
Dalibor Hon <hon.dalibor@gmail.com>
2025-01-22 15:37:21 +0100

    Implement more components #3

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,9 +3,16 @@ import { toast } from "bulma-toast";
 declare module "@pagefind/default-ui";
 
 declare global {
-	interface Window {
-		toast: typeof toast;
+	interface ToastOptions {
+		message: string;
+		type?: "info" | "success" | "warning" | "error";
+		duration?: number; // in ms; 0 or negative to keep open
+		dismissible?: boolean; // show close button or not
 	}
+
+	interface Window {
+		showToast: (options: ToastOptions) => void;
+	  }
 
 	namespace App {
 		interface Locals {

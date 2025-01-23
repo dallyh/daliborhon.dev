8ef2e2a80e3a846349581987395fe1018b1e0bce
Dalibor Hon <hon.dalibor@gmail.com>
2025-01-23 21:11:58 +0100

    Implement more components #5

--- src/components/react/PageViews/UrlChart.tsx
+++ src/components/react/PageViews/UrlChart.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from "react/jsx-runtime";
 import { Bar, BarChart, LabelList, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import * as m from "$messages";
 
 type UrlChartProps = {
 	data: {
@@ -19,7 +21,7 @@ export default function UrlChart({ data }: UrlChartProps) {
 					mixBlendMode: "difference",
 					color: "transparent",
 					fontWeight: "lighter",
-					fill: "white",
+					fill: "oklch(var(--nc))",
 				}}
 			>
 				{value}
@@ -28,29 +30,34 @@ export default function UrlChart({ data }: UrlChartProps) {
 	};
 
 	return (
-		<ResponsiveContainer height={data.length * 50}>
-			<BarChart
-				margin={{
-					top: 10,
-					right: 0,
-					left: -58,
-					bottom: 0,
-				}}
-				layout="vertical"
-				data={data}
-			>
-				<YAxis type="category" dataKey="url" stroke="#FF0000" fontSize={12} tickLine={false} axisLine={false} tick={false} />
-				<XAxis type="number" hide />
-				<Tooltip
-					cursor={{ fill: "oklch(var(--p))", strokeWidth: 1 }}
-					wrapperStyle={{ maxWidth: "300px" }}
-					labelStyle={{ textWrap: "balance" }}
-					contentStyle={{ backgroundColor: "oklch(var(--B))" }}
-				/>
-				<Bar label={false} dataKey="pageviews" fill="oklch(var(--p))" radius={[4, 4, 4, 4]}>
-					<LabelList dataKey="url" position="insideLeft" content={({ x, y, value }: any) => <CustomLabel x={x} y={y} value={value} />} />
-				</Bar>
-			</BarChart>
-		</ResponsiveContainer>
+		<div className="mt-4">
+			{data.length === 0 && <p>{m.error__not_found_description()}.</p>}
+			{data.length !== 0 && (
+				<ResponsiveContainer height={data.length * 50}>
+					<BarChart
+						margin={{
+							top: 10,
+							right: 0,
+							left: -58,
+							bottom: 0,
+						}}
+						layout="vertical"
+						data={data}
+					>
+						<YAxis type="category" dataKey="url" stroke="#FF0000" fontSize={12} tickLine={false} axisLine={false} tick={false} />
+						<XAxis type="number" hide />
+						<Tooltip
+							cursor={{ fill: "oklch(var(--b3))", strokeWidth: 1 }}
+							wrapperStyle={{ maxWidth: "300px" }}
+							labelStyle={{ textWrap: "balance" }}
+							contentStyle={{ backgroundColor: "oklch(var(--b3))" }}
+						/>
+						<Bar label={false} dataKey="pageviews" fill="oklch(var(--p))" radius={[4, 4, 4, 4]}>
+							<LabelList dataKey="url" position="insideLeft" content={({ x, y, value }: any) => <CustomLabel x={x} y={y} value={value} />} />
+						</Bar>
+					</BarChart>
+				</ResponsiveContainer>
+			)}
+		</div>
 	);
 }

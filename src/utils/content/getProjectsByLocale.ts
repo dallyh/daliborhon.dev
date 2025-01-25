3cbbf9d4398df23ff8c665f0f25b5d98c1105b86
dallyh <hon.dalibor@gmail.com>
2025-01-25 21:43:14 +0100

    Cleanup #1

--- src/utils/content/getProjectsByLocale.ts
+++ src/utils/content/getProjectsByLocale.ts
@@ -1,12 +1,17 @@
 import { getCollection } from "astro:content";
-import type { AllowedLocales } from "@i18n-config";
 
-export async function getProjectsByLocale(locale: string, sort: boolean = true) {
-	const projects = await getCollection("projects", ({ data }) => {
+export async function getProjectsByLocale(locale: string, sort: boolean = true, max: number | undefined = undefined) {
+	let projects = await getCollection("projects", ({ data }) => {
 		return data.language === locale;
 	});
 
 	if (sort) {
-		return projects.sort((a, b) => new Date(b.data.startDate).valueOf() - new Date(a.data.startDate).valueOf());
+		projects = projects.sort((a, b) => new Date(b.data.startDate).valueOf() - new Date(a.data.startDate).valueOf());
 	}
+
+	if (max) {
+		projects = projects.slice(0, max);
+	}
+
+	return projects;
 }

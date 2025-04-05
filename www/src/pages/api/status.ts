16ff1c53a53c95c735d166166aaab1721046ae53
dallyh <hon.dalibor@gmail.com>
2025-04-05 22:20:28 +0200

    Fix status endpoint

--- www/src/pages/api/status.ts
+++ www/src/pages/api/status.ts
@@ -1,5 +1,10 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async () => {
-	return new Response(JSON.stringify({ status: "UP" }), { status: 200 });
+	return new Response(JSON.stringify({ status: "UP" }), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 };

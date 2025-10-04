e733674e434f6165c2c8e5dbd8c4ffbce4ad22de
Dalibor Hon <hon.dalibor@gmail.com>
2025-10-04 14:41:29 +0000

    Add a new blog post, fix post preview when there is no image

--- www/src/pages/[...lang]/blog/posts/[...slug]/preview.png.ts
+++ www/src/pages/[...lang]/blog/posts/[...slug]/preview.png.ts
@@ -1,4 +1,4 @@
-import { generateOgImageForPost } from "@utils/og";
+import { generatePreviewImageForPost } from "@utils/og";
 import type { APIContext } from "astro";
 import type { CollectionEntry } from "astro:content";
 
@@ -8,9 +8,13 @@ export async function GET({ params, props }: APIContext) {
 	if (!params.lang) {
 		return new Response(null, { status: 404 });
 	}
-
 	const post = props.post as CollectionEntry<"posts">;
-	return new Response(await generateOgImageForPost(post, params.lang), {
+
+	if (post.data.image || post.data.image !== "") {
+		return new Response(null, { status: 404 });
+	}
+
+	return new Response(await generatePreviewImageForPost(props.post, params.lang), {
 		headers: { "Content-Type": "image/png" },
 	});
 }
